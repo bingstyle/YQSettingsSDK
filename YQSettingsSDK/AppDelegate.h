32517a7eeb45587b20428b7a7b2e630e4d056628
魏心兵 <weixinbing@17m.club>
2018-01-05 16:47:42 +0800

    init

---
+++ YQSettingsSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQSettingsSDK
+//
+//  Created by weixb on 2018/1/5.
+//  Copyright © 2018年 weixb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
